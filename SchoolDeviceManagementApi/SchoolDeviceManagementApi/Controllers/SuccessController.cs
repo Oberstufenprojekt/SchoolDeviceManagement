1694fc1aebf9a7bb96ca3a58457d6bdba297cf95
Samofan <dinter.florian@googlemail.com>
2020-09-14 08:33:39 +0200

    Initial commit of SchoolDeviceManagementApi

---
+++ SchoolDeviceManagementApi/SchoolDeviceManagementApi/Controllers/SuccessController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolDeviceManagementApi.Controllers
+{
+    [ApiController]
+    [Route("success")]
+    public class SuccessController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "API is working correctly!";
+        }
+    }
+}
