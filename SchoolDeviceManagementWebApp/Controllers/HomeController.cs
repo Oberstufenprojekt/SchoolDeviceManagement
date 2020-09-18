c68e44efed0b0dc85a0094fba8ef90d76fb29d32
Samofan <dinter.florian@googlemail.com>
2020-09-18 23:35:03 +0200

    Add reservations view

--- SchoolDeviceManagementWebApp/Controllers/HomeController.cs
+++ SchoolDeviceManagementWebApp/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace SchoolDeviceManagementWebApp.Controllers
             return View();
         }
 
+        public IActionResult Reservations()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
