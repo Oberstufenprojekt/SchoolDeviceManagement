676e30c8db78a4e73eec0c2bbd47538bdd37831a
angernit <arianitxyz@hotmail.com>
2020-09-27 12:40:25 +0200

    jetzt wird nur das LogIn Page angezeigt, ist das richtig ?

---
+++ SchoolDeviceManagementWebApp/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace SchoolDeviceManagementWebApp.Areas.Identity.Pages.Account
+{
+    
+    [AllowAnonymous]
+    public class RegisterConfirmationModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
