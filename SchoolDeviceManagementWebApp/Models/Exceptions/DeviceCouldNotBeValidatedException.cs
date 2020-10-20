af388ea2b0eb015c1431601f8ef3dc917fe73365
Samofan <dinter.florian@googlemail.com>
2020-10-20 13:57:36 +0200

    Add DeviceCouldNotBeValidatedException

---
+++ SchoolDeviceManagementWebApp/Models/Exceptions/DeviceCouldNotBeValidatedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SchoolDeviceManagementWebApp.Models.Exceptions
+{
+    /// <summary>
+    /// An exception that is thrown if a device could not be validated.
+    /// </summary>
+    public class DeviceCouldNotBeValidatedException : Exception
+    {
+        /// <summary>
+        /// Basic constructor with message.
+        /// </summary>
+        public DeviceCouldNotBeValidatedException() 
+            : base("Error while validating the device.")
+        {
+            
+        }
+    }
+}
--- SchoolDeviceManagementWebApp/Services/AddDeviceService.cs
+++ SchoolDeviceManagementWebApp/Services/AddDeviceService.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Runtime.InteropServices;
 using SchoolDeviceManagementWebApp.Data;
 using SchoolDeviceManagementWebApp.Models;
+using SchoolDeviceManagementWebApp.Models.Exceptions;
 
 namespace SchoolDeviceManagementWebApp.Services
 {
@@ -31,21 +31,10 @@ namespace SchoolDeviceManagementWebApp.Services
         /// <param name="deviceToAdd">The device to add.</param>
         public void Add(Device deviceToAdd)
         {
-            // TODO: Test this implementation. 
-            var context = new ValidationContext(deviceToAdd);
-            var validationResults = new List<ValidationResult>();
-
-            bool isValid = Validator.TryValidateObject(deviceToAdd, context, validationResults, true);
+           if (!Validated(deviceToAdd)) throw new DeviceCouldNotBeValidatedException();
             
-            
-            
-            
-            
-            if (Validated(deviceToAdd))
-            {
-                _dbContext.Add(deviceToAdd);
-                _dbContext.SaveChanges();
-            }
+            _dbContext.Add(deviceToAdd);
+            _dbContext.SaveChanges();
         }
 
         /// <summary>
@@ -56,7 +45,13 @@ namespace SchoolDeviceManagementWebApp.Services
         /// <returns>True if the device can be inserted into the database.</returns>
         private bool Validated(Device deviceToValidate)
         {
-            throw new System.NotImplementedException();
+            // TODO: Test this implementation. 
+            
+            // TODO: Check if device is non existent.
+            var context = new ValidationContext(deviceToValidate);
+            var validationResults = new List<ValidationResult>();
+            
+            return Validator.TryValidateObject(deviceToValidate, context, validationResults, true);
         }
     }
 }
